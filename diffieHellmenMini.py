d51bbaea170f9f961b40adef29d3d517f30c9f53
thomkaar <thomkaar@gmail.com>
2019-06-04 11:02:24 -0700

    Initial commit

---
+++ diffieHellmenMini.py
@@ -0,0 +1,16 @@
+# This file is an implementation of a Diffie Hellmen key exchange with using a 
+# p value of 37 and a g value of 5 
+from Crypto import Random
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+
+# execute key exchange
+def main():
+   
+
+
+
+
+
+if __name__ == '__main__':
+   main()
