cb4fef718d80b876a2c5d7f23fb23bcb76cd2768
Kellie Banzon <kelliebanzon@gmail.com>
2019-06-08 16:34:42 -0700

    fixed symmetric key generation

--- diffieHellmenMini.py
+++ diffieHellmenMini.py
@@ -9,6 +9,7 @@ import Crypto.Random.random as rand
 def main():
    p = 37
    g = 5
+   
    # A = g**(randomInt) % p
    # a = randomInt
    A, a = pickAB(p, g)
@@ -19,14 +20,20 @@ def main():
 
    s_input_Alice = sym_input(B, a, p)
    s_input_Bob = sym_input(A, b, p)
-   hash = SHA256.new()
-   sym_key_Alice = generate_key(hash, bytes(s_input_Alice))
-   sym_key_Bob = generate_key(hash, bytes(s_input_Bob)) 
-
+   sym_key_Alice = generate_key(SHA256.new(), bytes(s_input_Alice))
+   sym_key_Bob = generate_key(SHA256.new(), bytes(s_input_Bob))
+   print('Alice Key: {}\nBob Key: {}'.format(sym_key_Alice, sym_key_Bob))
+   print()
    # We now have symmetric keys
    
    alice_cipher = buildCipher("Hi Bob!", sym_key_Alice)
-   bob_cipher = buildCipher("Hi Alice!", sym_key_Alice)
+   bob_cipher = buildCipher("Hi Alice!", sym_key_Bob)
+   print('Alice Hi Encrypted: {}\nBob Hi Encrypted: {}'.format(alice_cipher, bob_cipher))
+   
+   alice_decrypted = aes_decrypt(alice_cipher, sym_key_Bob)
+   bob_decrypted = aes_decrypt(bob_cipher, sym_key_Alice)
+   print(alice_decrypted)
+   print(bob_decrypted)
 
 # Given a p and g value choose a random int between 1 and p-2 
 # and return (g**random_int) % p and random_int
@@ -46,11 +53,41 @@ def generate_key(hash, s_input):
 # Given a message and a key that is 16 bytes long, build an AES-Cipher in CBC mode
 def buildCipher(message, key):
    rand_file = Random.new()
-   aes_obj = AES.new(key, AES.MODE_CBC, rand_file.read(16))
-   cipher = ""
-   i = 0
-   
+   iv = rand_file.read(16)
+   cipher = aes_encrypt(message, key, iv)
    return cipher
+
+# AES encrypt the message with the given key and IV
+def aes_encrypt(message, key, iv):
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    padded_message = pad_aes_message(message)
+    return encryptor.encrypt(padded_message)
+        
+def aes_decrypt(message, key, iv):
+    decryptor = AES.new(key, AES.MODE_CBC, iv)
+    return decryptor.decrypt(message)
+
+def pad_aes_message(message):
+    acc = ''
+    i = 0
+    j = 16
+    while len(message[i:j]) == 16:
+        acc += message[i:j]
+        i += 16
+        j += 16
+    acc += pkcs7(message[i:j])
+    return acc
+        
+def pkcs7(plaintext):
+   if len(plaintext) == 16:
+      pad_len = 16
+   else:
+      pad_len = 16 - len(plaintext)
+   # pad_char = pad_len.to_bytes(1, byteorder='big')
+   pad_char = ';'
+   for i in range(pad_len):
+      plaintext += pad_char
+   return plaintext
 
 if __name__ == '__main__':
    main()
